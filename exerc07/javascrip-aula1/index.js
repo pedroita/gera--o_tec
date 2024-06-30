aa1786757bb2fbd1f118d338391b3138e2ce518e
Pedro Ítalo <pedroitalocampos@gmail.com>
2024-06-29 23:53:26 -0300

    add requires

---
+++ exerc07/javascrip-aula1/index.js
@@ -0,0 +1,8 @@
+// document.write("<h1>Hello, wold</h1>")
+let numero1 = Number(prompt('Digite um numero'));
+let numero2 = Number(prompt('Digite um numero'));
+let resulado = numero1+numero2
+
+alert(resulado)
+console.log(` Resultado da soma foi : ${resulado}`)
+document.write(`<h1> Resultado da soma foi : ${resulado} </h1>`)
